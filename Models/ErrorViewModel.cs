302f3cb8b9a333a2f512d2522a1e0a6a7fd462be
gestinl <loic.gestin34@gmail.com>
2022-12-01 17:28:26 +0100

    Hello world

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace NuitInfo2022.Models
 {
     public class ErrorViewModel
     {
+        "Test"
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
